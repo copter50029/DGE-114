30188d04c52beb0a0b019832852ae30e3eb3ee90
copter50029 <152718101+copter50029@users.noreply.github.com>
2024-07-30 11:06:19 +0700

    update libary

--- random_ball_itertools/random_ball.py
+++ random_ball_itertools/random_ball.py
@@ -2,19 +2,19 @@ import itertools
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
-import pandas as pd
+
 #define the directory where the images are stored
 image_directory = os.path.join(os.path.dirname(__file__), 'color_ball')
 
 #define the colors and corresponding images fliename
 
 color_images = {
-    'red': os.path.join(os.path.dirname(__file__), 'color_ball/red_ball.png'),
-    'blue': os.path.join(os.path.dirname(__file__), 'color_ball/blue_ball.png'),
-    'green': os.path.join(os.path.dirname(__file__), 'color_ball/green_ball.png'),
-    'yellow': os.path.join(os.path.dirname(__file__), 'color_ball/yellow_ball.png'),
-    'orange': os.path.join(os.path.dirname(__file__), 'color_ball/orange_ball.png'),
-    'purple': os.path.join(os.path.dirname(__file__), 'color_ball/purple_ball.png'),
+    'red': os.path.join(os.path.dirname(__file__), 'color_ball/red_ball.jpg'),
+    'blue': os.path.join(os.path.dirname(__file__), 'color_ball/blue_ball.jpg'),
+    'green': os.path.join(os.path.dirname(__file__), 'color_ball/green_ball.jpg'),
+    'yellow': os.path.join(os.path.dirname(__file__), 'color_ball/yellow_ball.jpg'),
+    'orange': os.path.join(os.path.dirname(__file__), 'color_ball/orange_ball.jpg'),
+    'purple': os.path.join(os.path.dirname(__file__), 'color_ball/purple_ball.jpg'),
 
 }
 
@@ -30,18 +30,12 @@ def plot_permutation(perm,index):
         ax[i].imshow(img)
         ax[i].axis('off')
         ax[i].set_title(color)
+    
     plt.suptitle(f'Permutation {index+1}', fontsize=16)
     plt.show()
 
 for index, perm in enumerate(permutations):
     plot_permutation(perm, index)
-# Convert permutations to a DataFrame for easier CSV export
-df_permutations = pd.DataFrame(permutations, columns=[f'Color {i+1}' for i in range(len(colors))])
 
-# Define the CSV file path
-csv_file_path = os.path.join(os.path.dirname(__file__), 'permutation.csv')
 
-# Export the DataFrame to a CSV file
-df_permutations.to_csv(csv_file_path, index=False)
-
-print(f'Permutations have been saved to {csv_file_path}')
+ 
